7115de8d07def85992382e83ee1bc31eb2a56b66
Yifei Xue <yifei.van.xue@gmail.com>
2017-12-07 15:33:27 +0800

    Update commit ID and add openstack git repo
    Signed-off-by: Yifei Xue <yifei.van.xue@gmail.com>

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ systemctl mask firewalld
 
 mkdir -p /opt/git/
 cd /opt/git/
-wget artifacts.opnfv.org/compass4nfv/package/openstack.tar.gz
-tar -zxvf openstack.tar.gz
-rm -rf openstack.tar.gz
+wget artifacts.opnfv.org/compass4nfv/package/openstack_pike.tar.gz
+tar -zxvf openstack_pike.tar.gz
+rm -rf openstack_pike.tar.gz
 #cd openstack
 #git clone https://github.com/openstack/tacker.git -b stable/pike
 #cd tacker
@@ -23,7 +23,8 @@ cd /opt/openstack-ansible
 
 #git checkout b962eed003580ee4c3bd69da911f20b3905a9176
 #git checkout da37351ca0a96ed38de72f3e00a7549a024cb810
-git checkout 71110d6bc0f459b668948aca185139c1d79f0452
+#git checkout 71110d6bc0f459b668948aca185139c1d79f0452
+git checkout 16c69046bfd90d1b984de43bc6267fece6b75f1c
 
 git checkout -b stable/pike
 
@@ -47,8 +48,8 @@ rm -f /usr/local/bin/ansible-playbook
 cd /opt/openstack-ansible/scripts/
 python pw-token-gen.py --file /etc/openstack_deploy/user_secrets.yml
 
-#cd /opt/openstack-ansible/playbooks/inventory/group_vars
-#sed -i 's/#repo_build_git_cache/repo_build_git_cache/g' repo_all.yml
+cd /opt/openstack-ansible/group_vars
+sed -i 's/#repo_build_git_cache/repo_build_git_cache/g' repo_all.yml
 
 cp /opt/setup-complete.yml /opt/openstack-ansible/playbooks/
 echo "- include: setup-complete.yml" >> /opt/openstack-ansible/playbooks/setup-infrastructure.yml
